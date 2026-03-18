eec7a54376294a569161ad9e3ab07db2bfc7fe77
Hector <hfcloliveira@gmail.com>
2026-03-18 20:40:48 -0300

    Atividade 5

---
+++ Uni4Comp05.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Uni4Comp05 {
+    
+    /*
+    Enunciado: Escreva um programa que valide um cupom de desconto em formato de texto. 
+    Leia o nome do cupom digitado pelo usuário. Se o cupom for exatamente igual a "SOUFURB", 
+    o programa deve exibir a mensagem: "Desconto de 15% aplicado com sucesso!".
+    
+    
+    
+    */
+
+    public static void main(String[] args) {
+        
+        String cupom;
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe o tipo de cupom:");
+        cupom = leitor.nextLine();
+
+        if (cupom == "SOUFURB") {
+
+            System.out.println("Desconto de 15% aplicado com sucesso!");
+
+        }
+
+        leitor.close();
+
+    }
+}
