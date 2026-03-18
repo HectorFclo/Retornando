8a88ad3b74bd5ea665615b96997e71e6141631fe
Hector <hfcloliveira@gmail.com>
2026-03-18 19:52:33 -0300

    ajuste

--- Uni4Comp01.java
+++ Uni4Comp01.java
@@ -1,30 +1,30 @@
+
 /*Enunciado: Uma loja virtual deseja dar um brinde para categorias específicas de clientes. 
 Escreva um programa que leia um caractere representando o tipo de cupom ('S' para Especial, 'C' para Comum). 
 Se o cupom for igual ('==') a 'S', o programa deve exibir a mensagem: "Cupom validado! 
 Você ganhou um adesivo exclusivo." */
 import java.util.Scanner;
+
 public class Uni4Comp01 {
-public static void main(String[] args) {
-    char cupom;
-    
-    Scanner leitor = new Scanner(System.in);
+    public static void main(String[] args) {
+        char cupom;
 
-    System.out.println("Informe o tipo de cupom:");
-cupom = leitor.next().charAt(0);
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Informe o tipo de cupom:");
+        cupom = leitor.next().charAt(0);
 
+        if (cupom == 'S') {
 
-if (cupom == 'S'){
+            System.out.println("Cupom validado! Você ganhou um adesivo exclusivo.");
 
-System.out.println("Cupom validado! Você ganhou um adesivo exclusivo.");
+        }
+        if (cupom == 'C') {
 
-}if (cupom == 'C'){
+        }
 
+        leitor.close();
 
-}
-
-leitor.close();
-
-}
+    }
 
 }
--- Uni4Comp02.java
+++ Uni4Comp02.java
@@ -1,33 +1,47 @@
 import java.util.Scanner;
 
 public class Uni4Comp02 {
-/*
-Um sistema de monitoramento de TI verifica o status de um servidor através de códigos numéricos. 
-O código 1 significa que o sistema está "OK". Escreva um algoritmo que leia um número inteiro representando o status atual. 
-Se o status for diferente ('!=') de 1, o programa deve exibir o alerta: "ATENÇÃO: Sistema operando com instabilidade!"
+    /*
+     * Um sistema de monitoramento de TI verifica o status de um servidor através de
+     * códigos numéricos.
+     * O código 1 significa que o sistema está "OK". Escreva um algoritmo que leia
+     * um número inteiro representando o status atual.
+     * Se o status for diferente ('!=') de 1, o programa deve exibir o alerta:
+     * "ATENÇÃO: Sistema operando com instabilidade!"
+     * 
+     * Entrada:
+     * Ler(status)
+     * 
+     * 
+     * Processa:
+     * 
+     * Se status !=1
+     * print: "ATENÇÃO: Sistema operando com instabilidade!"
+     * 
+     * 
+     * Saida:
+     * imprimir(alerta)
+     * 
+     * 
+     */
 
-*/
+    public static void main(String[] args) {
 
-public static void main(String[] args) {
-    
-int status;
+        int status;
 
+        Scanner leitor = new Scanner(System.in);
 
-Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe o status atual:");
+        status = leitor.nextInt();
 
-    System.out.println("Informe o status atual:");
-status = leitor.nextInt();
+        if (status == 1) {
 
+        } else {
+            System.out.println("Sistema operando com instabilidade!");
 
+        }
 
-if (status == 1){
+        leitor.close();
+    }
 
-}else{
-System.out.println("Sistema operando com instabilidade!");
-
-}
-
-leitor.close();
-}
-   
 }
